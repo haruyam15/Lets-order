e819c8f9d0a4a3dae3807ca73894b83ab8b2629f
Haeun <haruyam15@gmail.com>
2025-10-28 19:59:22 +0900

    feat: 디테일 페이지 ui 개발

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,3 +1,142 @@
+import { Button } from '@/components/ui/button';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+import { Separator } from '@/components/ui/separator';
+import { Check, ChevronLeft, Minus, Plus } from 'lucide-react';
+
 export function DetailPage() {
-  return <div>디테일</div>;
+  return (
+    <div>
+      <div className="w-full px-3">
+        <div className="sticky top-0 bg-white">
+          <Button variant="ghost" className="mt-3 size-10 md:size-15">
+            <ChevronLeft className="md:size-15 size-10" />
+          </Button>
+        </div>
+
+        <div className="w-full h-[200px] overflow-hidden mt-2 mb-5">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
+            alt=""
+            className="object-contain size-full"
+          />
+        </div>
+        <dl>
+          <dt className="mb-2 text-xl font-bold">스코프 (Scope)</dt>
+          <dd className="text-base">
+            변수 접근 범위를 한 번에 정리해요. 블록/함수/전역 스코프 차이를
+            예제로 이해합니다.
+          </dd>
+        </dl>
+
+        <div className="flex justify-between mt-9">
+          <p className="text-xl font-semibold text-gray-800">3500원</p>
+          <div className="flex items-center gap-3">
+            <Button className="size-10" variant="secondary">
+              <Minus />
+            </Button>
+            <p>1</p>
+            <Button className="size-10" variant="secondary">
+              <Plus />
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <Separator className="py-2 my-9 bg-secondary" />
+
+      <div className="w-full px-3">
+        <p className="text-lg font-semibold">난이도</p>
+        <ul className="grid grid-cols-2 gap-2 my-5 text-base">
+          <li className="py-5 text-center rounded-md bg-secondary">초급</li>
+          <li className="py-5 text-center rounded-md bg-secondary">중급</li>
+          <li className="py-5 text-center rounded-md bg-secondary">고급</li>
+        </ul>
+      </div>
+
+      <Separator className="my-9" />
+
+      <div className="w-full px-3">
+        <p className="text-lg font-semibold">추가 학습(최대 2개)</p>
+        <ul className="flex flex-col gap-5 my-5 text-base">
+          <li className="flex items-center justify-between">
+            <div>
+              <p>면접 질문 세트</p>
+              <span className="text-sm text-zinc-600">+ 0원</span>
+            </div>
+            <div>
+              <Check className="text-orange-400" />
+            </div>
+          </li>
+          <li className="flex items-center justify-between">
+            <div>
+              <p>시각화 다이어그램</p>
+              <span className="text-sm text-zinc-600">+ 500원</span>
+            </div>
+            <div>
+              <Check className="text-zinc-300" />
+            </div>
+          </li>
+          <li className="flex items-center justify-between">
+            <div>
+              <p>퀴즈 10문항</p>
+              <span className="text-sm text-zinc-600">+ 500원</span>
+            </div>
+            <div>
+              <Check className="text-zinc-300" />
+            </div>
+          </li>
+          <li className="flex items-center justify-between">
+            <div>
+              <p>심화 링크 모음</p>
+              <span className="text-sm text-zinc-600">+ 1000원</span>
+            </div>
+            <div>
+              <Check className="text-zinc-300" />
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <Separator className="my-9" />
+
+      <div className="w-full px-3">
+        <p className="text-lg font-semibold">학습 형태</p>
+
+        <Drawer>
+          <DrawerTrigger>옵션을 선택해주세요</DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader>
+              <DrawerTitle>학습 형태를 선택해주세요</DrawerTitle>
+            </DrawerHeader>
+            <ul className="flex flex-col gap-5 px-5 my-5 text-base">
+              <li className="flex items-center justify-between">
+                <div>
+                  <p>요약 리딩</p>
+                  <span className="text-sm text-zinc-600">+ 0원</span>
+                </div>
+              </li>
+              <li className="flex items-center justify-between">
+                <div>
+                  <p>핵심 예제</p>
+                  <span className="text-sm text-zinc-600">+ 500원</span>
+                </div>
+              </li>
+              <li className="flex items-center justify-between">
+                <div>
+                  <p>프로젝트 실습</p>
+                  <span className="text-sm text-zinc-600">+ 1000원</span>
+                </div>
+              </li>
+            </ul>
+          </DrawerContent>
+        </Drawer>
+      </div>
+    </div>
+  );
 }
