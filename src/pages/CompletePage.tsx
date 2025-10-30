67d7ee6e3f48449629bf64e7858ac429adc3665d
Haeun <haruyam15@gmail.com>
2025-10-30 18:08:53 +0900

    feat: 주문완료페이지 UI 구현

--- src/pages/CompletePage.tsx
+++ src/pages/CompletePage.tsx
@@ -1,3 +1,22 @@
+import FixedButton from '@/components/common/FixedButton';
+import Title from '@/components/common/Title';
+import { CircleCheck } from 'lucide-react';
+import { NavLink } from 'react-router-dom';
+
 export function CompletePage() {
-  return <div>완료</div>;
+  return (
+    <div className="flex flex-col w-full h-screen md:w-2xl">
+      <Title text="주문완료" backBtn={false} />
+
+      <div className="flex flex-col items-center justify-center flex-1 w-full gap-2 px-3">
+        <CircleCheck className="mb-5 text-orange-500 size-15" />
+        <p className="text-xl font-bold">주문이 완료되었습니다</p>
+        <span className="text-base text-zinc-600">4개 18,000원</span>
+      </div>
+
+      <FixedButton>
+        <NavLink to="/menu">완료우 !</NavLink>
+      </FixedButton>
+    </div>
+  );
 }
